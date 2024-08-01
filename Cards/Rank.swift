443896305680957330fd94d3d9a36bb618b9f304
Alonso del Arte <alonso.delarte@gmail.com>
2024-08-01 17:02:38 -0400

    Passing symbol( ) test

--- Cards/Rank.swift
+++ Cards/Rank.swift
@@ -36,7 +36,21 @@ enum Rank : CaseIterable {
     case king
     
     func symbol() -> String {
-        "NOT IMPLEMENTED YET"
+        switch (self) {
+        case .ace: "A"
+        case .two: "2"
+        case .three: "3"
+        case .four: "4"
+        case .five: "5"
+        case .six: "6"
+        case .seven: "7"
+        case .eight: "8"
+        case .nine: "9"
+        case .ten: "10"
+        case .jack: "J"
+        case .queen: "Q"
+        case .king: "K"
+        }
     }
     
     func word() -> String {
