cd251fdfce15ad0fe298096ae7c16a646957dfff
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-10 17:59:53 -0400

    New test class

---
+++ CardsTests/PlayingCard.swift
@@ -0,0 +1,14 @@
+//
+//  PlayingCard.swift
+//  BlackJackJack
+//
+//  Created by Alonso del Arte on 9/10/24.
+//
+
+import Foundation
+
+struct PlayingCard {
+    
+    // TODO: init(rank, suit)
+    
+}
